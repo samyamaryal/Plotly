39993beab59b1f5739c299d054da0e88bf98c118
Samyam Aryal <samyam.aryal01@gmail.com>
2023-05-02 17:47:57 +0545

    Tried out a few more features of DCC, including Graphs.

--- dcc_basics.py
+++ dcc_basics.py
@@ -1,5 +1,6 @@
 from dash import Dash, html, dcc
 import pandas as pd
+import plotly.express as px
 
 app = Dash(__name__)
 
@@ -9,11 +10,33 @@ df = pd.read_csv('intro_bees.csv')
 app.layout = html.Div([
     
     html.H1("Dashboard using dash  "),
-    dcc.P("This is a dropdown with a single choice option"),
-    dcc.Dropdown(['Option 1', 'Option 2', 'Option 3']),
+    html.P("This is a dropdown with a single choice option"),
+    dcc.Dropdown(['Option 1', 'Option 2', 'Option 3'], value = 'Option 1', id='Single Dropdown'),
     
     html.P("This is a dropdown with multiple choices available"),
-    dcc.Dropdown(['Option 1', 'Option 2', 'Option 3'], multi = True)
+    dcc.Dropdown(['Option 1', 'Option 2', 'Option 3'], value='Option 2', multi=True, id='Multi Dropdown'),
+    
+    html.P("This is a slider with a default value"),
+    dcc.Slider(min=1, max=10, step=1, value=3, id='Trial Slider'),
+    
+    html.P("This is an input field"),
+    dcc.Input(id='Inputfield', value='eg'),
+    
+    html.P("This is a checklist"),
+    dcc.Checklist(['Checkbox 1', 'Checkbox 2', 'Checkbox 3'], value='Checkbox1'),
+    
+    html.P("These are Radio items"),
+    dcc.RadioItems(['Checkbox 1', 'Checkbox 2', 'Checkbox 3'], value='Checkbox1', inline=False),
+    
+    html.Br(),
+    # Now, on to what might be the most important feature of DCC: Graphs
+    # Any Plotly figure can be added as a Graph in a Dashboard.
+    
+    dcc.Graph(figure = px.scatter(x=[0, 1, 2, 3, 4], 
+                                  y=[0, 1, 4, 9, 16]
+                                  ))
     ])
 
 
+if __name__ == '__main__':
+    app.run_server(debug= False)
